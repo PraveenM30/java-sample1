97c90ec3abbe101dabd37ad3e538707f6d4ab39f
karan.k@gmail.com <praveen.mohansundar9@gmail.com>
2024-04-30 18:43:58 +0530

    third commit

--- src/test1/sample1.java
+++ src/test1/sample1.java
@@ -4,6 +4,8 @@ public class sample1 {
     public static void main(String[] args) {
         System.out.println("1111111");
         System.out.println("new22222222");
-        System.out.println("updated directly in GitHub")
+        System.out.println("updated directly in GitHub");
+        System.out.println("33333 in Intelij");
+
     }
 }
